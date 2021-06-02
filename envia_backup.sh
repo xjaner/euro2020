78ccc994b80d48a84b76f823c68755f4783c3308
Xavier Janer <xjaner@gmail.com>
2021-06-02 07:00:15 +0200

    Arreglat l'script de backup per executar-se des de crontab

--- envia_backup.sh
+++ envia_backup.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 backup_file=backup_"$(date +%F)".sql
 cd /home/xavier/dev/euro2020
-/usr/local/bin/docker-compose exec db pg_dump euro2021 -U postgres -f /pg_backups/"$backup_file"
+/usr/local/bin/docker-compose exec -T db pg_dump euro2021 -U postgres -f /pg_backups/"$backup_file"
 printf "Backup del dia!" | mpack -a -s "Eurobackup" -d /dev/stdin -m 0  /home/xavier/dev/data/euro2021/pg_backups/"$backup_file" xjaner@gmail.com
